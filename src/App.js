279199e416ab6d06b7bb9775bdf0876eccfd9bff
hachi-plum <debayuru0901@gmail.com>
2024-12-11 14:09:12 +0900

    画像とアイコン表示

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FaHome, FaInfoCircle } from "react-icons/fa";
+import coffee from "./assets/coffee.jpg";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -12,12 +14,28 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="app-content">
+        <h1>
+          <FaHome style={{ color: "blue", marginRight: "10px" }} />
+          Welcome to Our Website
+        </h1>
+        <p>
+          <FaInfoCircle style={{ color: "green", marginRight: "10px" }} />
+          This is a demo React application.
+        </p>
+
+        <img 
+          src={coffee} 
+          alt="Coffee" 
+          style={{ width: "300px", borderRadius: "10px", marginTop: "20px" }} 
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
       <Footer />
     </Router>
   );
